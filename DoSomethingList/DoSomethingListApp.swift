9dd4b8cc964c0d98816525d201f19f0486f7dcfc
Matias Villar <matiase.villar@gmail.com>
2023-10-11 10:43:03 -0600

    Initial Commit

---
+++ DoSomethingList/DoSomethingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoSomethingListApp.swift
+//  DoSomethingList
+//
+//  Created by admin on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DoSomethingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
